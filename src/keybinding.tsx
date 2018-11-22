8b4a0de989f4b260bda4ac0f27c4954fd06a1676
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-11-22 13:53:08 +0100

    Rewrite the library in TypeScript

---
+++ src/keybinding.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
+interface KeybindingProps {
+  onKey: (e: KeyboardEvent) => void;
+  type: 'keydown' | 'keyup';
+  target: string | HTMLElement | Document | Window;
+  preventInputConflict: boolean;
+  preventDefault: boolean;
+  stopPropagation: boolean;
+}
+
+class Keybinding extends React.Component<KeybindingProps> {
+  static defaultProps = {
+    type                 : 'keydown',
+    target               : document, // Probably will make server-side rendering crash
+    preventInputConflict : false,
+    preventDefault       : false,
+    stopPropagation      : false
+  };
+
+  static propTypes = {
+    onKey: PropTypes.func,
+    type: PropTypes.string,
+    target: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    preventInputConflict: PropTypes.bool,
+    preventDefault: PropTypes.bool,
+    stopPropagation: PropTypes.bool
+  };
+
+  targetsBlacklist: string[] = ['textarea', 'input', 'select'];
+
+  constructor(props: KeybindingProps) {
+    super(props);
+    this.state = {};
+    this.onKey = this.onKey.bind(this);
+  }
+
+  render() {
+    return null;
+  }
+
+  onKey (e: Event) { // is actually a KeyboardEvent
+    if(this.props.preventDefault) e.preventDefault();
+    if(this.props.stopPropagation) e.stopPropagation();
+
+    const target = e.target as HTMLElement | null;
+
+    if (target) {
+      const canDispatch = !(this.props.preventInputConflict && (this.targetsBlacklist.indexOf(target.tagName.toLowerCase()) > -1));
+
+      if (canDispatch && this.props.onKey) this.props.onKey(e as KeyboardEvent);
+    }
+  }
+
+  componentDidMount() {
+    const { target, type } = this.props;
+
+    if(typeof target === 'string') {
+      const element = document.querySelector(target)
+      if (!element) throw (new Error(`Selector "${target}" returned null (on keybinding mount)`));
+
+      element.addEventListener(type, this.onKey);
+    }
+
+    else if(typeof target === 'object') {
+      target.addEventListener(type, this.onKey);
+    };
+  }
+
+  componentWillUnmount() {
+    const { target, type } = this.props;
+
+    if(typeof target === 'string') {
+      const element = document.querySelector(target);
+
+      if (!element) throw (new Error(`Selector "${target}" returned null (on keybinding unmount)`));
+
+      element.removeEventListener(type, this.onKey);
+    }
+
+    else if(typeof target === 'object') {
+      target.removeEventListener(type, this.onKey);
+    }
+  }
+}
+
+export default Keybinding;
